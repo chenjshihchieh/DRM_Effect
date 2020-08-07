804b30d20866263f50beab9ba3c2ede2a9394bb1
unknown <chen.j.shihchieh@gmail.com>
2020-08-06 22:24:59 -0700

    basic layout with necessary supplementary materials.

---
+++ server.R
@@ -0,0 +1,16 @@
+function(input, output){
+  ##Front page
+    ##Select on one of the four? 
+    
+  ##Instruction
+  
+  ##Listening to stimuli
+  
+  ##Recalling as many words as they can
+    ##They have 2 minutes to recall  
+  
+  ##Distractor task math questions
+    ##5 minute interval to answer as many questions as they can
+  
+  ##Slider to judge whether they recognize the word (for selected words)
+}
