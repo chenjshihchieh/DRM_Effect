f9738cb85c6c18d42562b496dc2ee7b8cd4d2d21
Jack Chen <chen.j.shihchieh@gmail.com>
2020-08-09 10:34:23 -0700

    forming the basic foundatino of the dynamic layout

--- server.R
+++ server.R
@@ -1,16 +1,18 @@
 function(input, output){
-  ##Front page
+  renderUI(
+    ##Front page
     ##Select on one of the four? 
     
-  ##Instruction
-  
-  ##Listening to stimuli
-  
-  ##Recalling as many words as they can
+    ##Instruction
+    
+    ##Listening to stimuli
+    
+    ##Recalling as many words as they can
     ##They have 2 minutes to recall  
-  
-  ##Distractor task math questions
+    
+    ##Distractor task math questions
     ##5 minute interval to answer as many questions as they can
-  
-  ##Slider to judge whether they recognize the word (for selected words)
+    
+    ##Slider to judge whether they recognize the word (for selected words)
+  )
 }
--- ui.R
+++ ui.R
@@ -1,3 +1,7 @@
 fluidPage(
-  
+  uiOutput(
+    ##Instruction
+    
+    ##Audio  
+  )
 )
